a15231cc2e655710a925395a53b71417c4c91f6c
BeneGuy <74668498+BeneGuy@users.noreply.github.com>
2020-11-18 16:54:45 +0100

    Test

--- Firefly.java
+++ Firefly.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 
@@ -7,6 +9,20 @@ public class Firefly
 {
     public static void main(String[] args){
         
-     System.out.println("Test");   
+        JFrame testo = new JFrame();
+        testo.setVisible(true);
+        testo.setSize(600, 300);
+        testo.setLocationRelativeTo(null);
+        testo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        troll(testo);
+     
+    }
+    
+    private static void troll(JFrame frame) {
+        for (int i = 0; i <= 1001; i = i + 100) {
+            Thread.sleep(1000);
+            frame.setSize(i, 500);
+        } 
     }
 }
